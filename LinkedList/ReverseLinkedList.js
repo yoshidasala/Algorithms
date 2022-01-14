a0f13e17db3c2729b92bfb254133446e84346476
Sala Yoshida <yoshidasala@gmail.com>
2022-01-14 10:15:47 -0500

    reverse linked list

--- LinkedList/ReverseLinkedList.js
+++ LinkedList/ReverseLinkedList.js
@@ -43,5 +43,7 @@ head.next = new LinkedList(1);
 head.next.next = new LinkedList(2);
 head.next.next.next = new LinkedList(3);
 head.next.next.next.next = new LinkedList(4);
+head.next.next.next.next.next = new LinkedList(5);
+console.log(reverseLinkedList(head))
 
 
