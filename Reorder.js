21fd7cd67a75f830672590dbbd649515244934c5
Sala Yoshida <yoshidasala@gmail.com>
2022-01-27 15:39:01 -0500

    new order optimized

--- Reorder.js
+++ Reorder.js
@@ -6,9 +6,11 @@ function sort(items, newOrder) {
   let result = []
   for (let i = 0; i < items.length; i++){
     let newIndex = newOrder[i]
-    result[newIndex] = items[i]
+      ;[newOrder[newIndex], newOrder[i]] = [newOrder[i], newOrder[newIndex]]
+      ;[items[newIndex],items[i]] =[items[i],items[newIndex]]
+
   }
-  return result
+  return items
 }
 
 console.log(sort(A,B))
