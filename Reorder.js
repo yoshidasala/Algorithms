be2335d11a9e54431b8c715e7e6314ed550329f6
Sala Yoshida <yoshidasala@gmail.com>
2022-01-27 15:36:47 -0500

    reoder

--- Reorder.js
+++ Reorder.js
@@ -3,10 +3,12 @@ const B = [1,   5,   4,   3,   2,   0]
 
 function sort(items, newOrder) {
 
+  let result = []
   for (let i = 0; i < items.length; i++){
-    let to = newOrder[i]
-
-      ;[[items[to],[items[items[i]]],[]]=[]
+    let newIndex = newOrder[i]
+    result[newIndex] = items[i]
   }
-
+  return result
 }
+
+console.log(sort(A,B))
