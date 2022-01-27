1adb7abf1b7745eb0d6c0f9f5123b8881824025e
Sala Yoshida <yoshidasala@gmail.com>
2022-01-26 19:54:22 -0500

    redorder

---
+++ Reorder.js
@@ -0,0 +1,12 @@
+const A = ['A', 'B', 'C', 'D', 'E', 'F']
+const B = [1,   5,   4,   3,   2,   0]
+
+function sort(items, newOrder) {
+
+  for (let i = 0; i < items.length; i++){
+    let to = newOrder[i]
+
+      ;[[items[to],[items[items[i]]],[]]=[]
+  }
+
+}
