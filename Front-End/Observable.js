0a4a5c3f095551ecc53fcc5bc0441fff436b2f83
Sala Yoshida <yoshidasala@gmail.com>
2022-02-02 10:58:21 -0500

    implementing observable

--- Front-End/Observable.js
+++ Front-End/Observable.js
@@ -1,47 +1,69 @@
 class Observable {
+
   constructor(setup) {
-    this.setup = setup;
+    this.setup = setup
   }
 
-  subscribe(subscriber) {
-    function unsubscribe() {
-      subscribed = false;
-    }
-
-    let subscribed = true;
-    const observer = {
+ subscribe(subscriber) {  // equivalent to fire
+    // a wrapper function/ object
+    // is used to share the closure of outer function and modify the logics
+    const subscriberWrapper = {
+      unsubscribed: false,
       next(value) {
-        if (!subscribed) {
-          return;
-        }
-
-        if (typeof subscriber === 'function') {
-          subscriber(value);
-        } else {
-          subscriber.next?.(value);
-        }
+        if (this.unsubscribed) return
+        // we are relying on the scope of subscriber
+        if (typeof subscriber === "function") return subscriber(value);
+        return subscriber.next ? subscriber.next(value) : null
       },
-      error(error) {
-        if (!subscribed) {
-          return;
-        }
-
-        unsubscribe();
-        subscriber.error?.(error);
+      error(value) {
+        if (this.unsubscribed) return
+        this.unsubscribe();
+        console.log(subscriber.error(value))
+        return subscriber.error ? subscriber.error(value) : null;
       },
       complete() {
-        if (!subscribed) {
-          return;
-        }
-
-        unsubscribe();
-        subscriber.complete?.();
+        if (this.unsubscribed) return;
+        this.unsubscribe();
+        return subscriber.complete ? subscriber.complete() : null
       },
-    };
-
-    this.setup(observer);
-    return {
-      unsubscribe,
-    };
+      unsubscribe() {
+        this.unsubscribed = true;
+      }
+    }
+    this.setup(subscriberWrapper);
+    return subscriberWrapper
   }
 }
+
+
+
+const setup = (observer)=> {
+  observer.next(1)
+  observer.next(2)
+  setTimeout(() => {
+    observer.error(3)
+    observer.next(4)
+    observer.complete()
+  }, 100)
+}
+
+const observable = new Observable(setup)
+
+
+
+const observer = {
+  next: (value) => {
+     console.log('we got a value', value)
+  },
+  error: (error) => {
+    console.log('we got an error', error)
+  },
+  complete: () => {
+    console.log('ok, no more values')
+  }
+}
+
+const sub = observable.subscribe(observer)
+
+
+// console.log(sub)
--- Front-End/ToBe().js
+++ Front-End/ToBe().js
@@ -1,25 +1,35 @@
-function myExpect(input_1) {
+function expect(input_1) {
+  //set the not to false first
   let not = false;
-  const obj = {
-      get not() {
+  return  {
+      get: not() {
         not = !not;
         return this;
-      },
-      toBe(input_2) {
+      }
+      toBe:(input_2) {
         if (not) {
           if (!Object.is(input_1, input_2)) {
-            Return true;
+            return true;
           }
-          Throw new Error(“FAILS”);
+          throw new Error("error");
         }
         if (Object.is(input_1, input_2)) {
-          Return true;
+          return true;
         }
-        Throw new Error(“FAILS”);
+        throw new Error("error");
       }
     }
-    return obj;
+
   }
 
 //NaN === NaN → false
-//+0 === -0 → true
+// //+0 === -0 → true
+// console.log(myExpect(3).toBe(3))
+// console.log(myExpect(4).toBe(3))
+
+
+// console.log(expect(3).not.toBe(3))
+// console.log(expect(4).not.toBe(3))
+
+
+console.log(expect(3).toBe(3))
