bb23efc6a5235a10d8cafac6991244c4316bd3ac
salayoshida <yoshidasala@gmail.com>
2022-03-16 02:36:58 -0700

    first duplicate

---
+++ Arrays/FirstDuplicate.js
@@ -0,0 +1,12 @@
+function firstDuplicateValue(array) {
+    const seen = new Set()
+    
+    for(const value of array){
+        if(seen.has(value))return value
+        seen.add(value)
+        
+    }
+        return -1
+     
+    
+    }
